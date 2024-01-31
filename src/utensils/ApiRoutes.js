5d1ec15ee9f5eff24e7dc90ec5509f3dae94e4f1
ajaychirme <ajaychirme84@gmail.com>
2024-02-01 03:16:52 +0530

    end point constant update

--- src/utensils/ApiRoutes.js
+++ src/utensils/ApiRoutes.js
@@ -1,9 +1,11 @@
-const loginRoute = "http://localhost:3003/auth/login";
-const registerRoute = "http://localhost:3003/auth/register";
-const addTaskRoute = "http://localhost:3003/task/add-task";
-const getAllTasksRoute = "http://localhost:3003/task/get-all-tasks";
-const deleteTaskRoute = "http://localhost:3003/task/delete-task/";
-const updateTaskRoute = "http://localhost:3003/task/update-task";
+const hostName = "http://localhost:3003";
+
+const loginRoute = `${hostName}/auth/login`;
+const registerRoute = `${hostName}/auth/register`;
+const addTaskRoute = `${hostName}/task/add-task`;
+const getAllTasksRoute = `${hostName}/task/get-all-tasks`;
+const deleteTaskRoute = `${hostName}/task/delete-task/`;
+const updateTaskRoute = `${hostName}/task/update-task`;
 export {
   loginRoute,
   registerRoute,
