a46d1b862ebbecad697e196d9844dd3423f78201
ajaychirme <ajaychirme84@gmail.com>
2024-02-01 22:06:32 +0530

    height fixed

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -141,6 +141,7 @@ function Dashboard() {
       <div
         style={{
           background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+          height:"100vh"
         }}
       >
         <Button
